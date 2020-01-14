6ebb73ac52782171aca9e9ea2cc903626231fd22
Unknown <akalamf14@gmail.com>
2020-01-13 17:15:14 -0700

    Folder organization, Created level 0, variables script

---
+++ 3D Game Project/Assets/Scripts/Assignments/Variables/Variables.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    //variables are containers
+    //Declaration
+    int number;
+    //Declaration and Initialization
+    int cookie = 5; //determine how many cookies are in the "cookie Container"
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        number = 15;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
