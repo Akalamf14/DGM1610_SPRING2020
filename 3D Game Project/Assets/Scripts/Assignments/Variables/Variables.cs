0faa8f1d4afdad1b063a3c14ed1c6006ad66f918
Unknown <akalamf14@gmail.com>
2020-01-14 19:03:30 -0700

    Unity Tutorial practicing variables and basic functions - multiplication.

--- 3D Game Project/Assets/Scripts/Assignments/Variables/Variables.cs
+++ 3D Game Project/Assets/Scripts/Assignments/Variables/Variables.cs
@@ -10,12 +10,24 @@ public class Variables : MonoBehaviour
     //Declaration and Initialization
     int cookie = 5; //determine how many cookies are in the "cookie Container"
 
+    int family = 6;
+
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log(family * 2); //Debug.Log() just shows numbers in game.
         number = 15;
+
+        family = multiplyByTwo(family);
+        Debug.Log(family);
     }
 
+    int multiplyByTwo(int number) //int number is just saying we have some number which we can put any interger in for that
+    {
+        int result;
+        result = number * 5;
+        return result;
+    }
     // Update is called once per frame
     void Update()
     {
