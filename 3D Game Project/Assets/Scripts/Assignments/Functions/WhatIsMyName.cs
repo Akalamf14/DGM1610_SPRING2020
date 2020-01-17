16c45f9646c7e01d618dc06348983a9ecc591f49
Unknown <akalamf14@gmail.com>
2020-01-17 09:53:03 -0700

    If Statements video tutorial, WhatsMyName functions practice script.

---
+++ 3D Game Project/Assets/Scripts/Assignments/Functions/WhatIsMyName.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhatIsMyName : MonoBehaviour
+{
+    string firstName = "Akala";
+    string middleName = "Marie";
+    string lastName = "Freeman";
+    string newLastName = "Escobar";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        fullName(firstName, middleName, lastName);
+        marriedName(firstName, middleName, newLastName);
+        
+    }
+
+    void fullName(string firstName, string middleName, string lastName)
+    {
+        string whatsMyName;
+        whatsMyName = firstName + middleName + lastName;
+        print(whatsMyName);
+    }
+
+    void marriedName(string firstName, string middleName, string newLastName)
+    {
+        string newFullName;
+        newFullName = firstName + middleName + newLastName;
+        print(newFullName);
+    }
+
+}
+
