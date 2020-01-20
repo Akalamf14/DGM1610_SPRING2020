48dc11e0227eaed0f0866127ced6f3f1ca38c4e7
Unknown <akalamf14@gmail.com>
2020-01-19 20:15:25 -0700

    MoreFunctions script - function practice number 4

---
+++ 3D Game Project/Assets/Scripts/Assignments/Functions/MoreFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoreFunctions : MonoBehaviour
+{
+    public int assignedHomework;
+    public int timeForHomework;
+    public int time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        homework(assignedHomework, timeForHomework, time);
+    }
+
+    void homework(int assignedHomework, int timeForHomework, int time)
+    {
+        int timeSpentOnHomework;
+        timeSpentOnHomework = time / (assignedHomework * timeForHomework);
+        print(timeSpentOnHomework);
+    }
+}
