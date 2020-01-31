521246c0f47a8c16a27f4169df694fca97c31500
Unknown <akalamf14@gmail.com>
2020-01-30 20:55:24 -0700

    MoveCamera script

--- 3D Game Project/Assets/Scripts/Assignments/Move/MoveCameraWithMouse.cs
+++ 3D Game Project/Assets/Scripts/Assignments/Move/MoveCameraWithMouse.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class MoveCameraWithMouse : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
 
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    
     // Update is called once per frame
     void Update()
     {
-        
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
     }
 }
