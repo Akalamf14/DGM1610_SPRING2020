0bcfb1707330a635edb51c267790111ec32bf10d
Unknown <akalamf14@gmail.com>
2020-01-31 23:43:49 -0700

    Notes for GetAxis - watched tutorials of destroy, getkey, getbutton

---
+++ 3D Game Project/Assets/Scripts/Assignments/Get.._/GetAxis.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetAxis : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        //GetKey and Get button both return a bool (true or false)
+        //GetAxis returns a float value between -1 and 1
+        // gravity determines how fast the value returns to zero, the larger the gravity the faster it returns to 0
+        // the higher the number the more responsive
+        // the lower the number the smoother the motion
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
