27d28475658bb787a3f3afe3797784e8335e3698
Unknown <akalamf14@gmail.com>
2020-02-01 23:27:42 -0700

    MouseClick Script with tutorial video

---
+++ 3D Game Project/Assets/Scripts/Assignments/On Mouse Down/MouseClickPractice.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClickPractice : MonoBehaviour
+{
+
+    // onMouseDown can detect a click on a collider or a GUI text element.
+
+    private Rigidbody rb;
+    
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMouseDown()
+    {
+       // Debug.Log("Clicked on the Door!");
+        rb.AddForce(-transform.forward * 500f);
+        rb.useGravity = true;
+    }
+}
