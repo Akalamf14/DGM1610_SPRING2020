f7beef221ea1b12606a075ab50d05e23914b0ecf
Unknown <akalamf14@gmail.com>
2020-02-04 17:16:36 -0700

    Unity Pickup lesson with scoreManager script and pickup script

---
+++ 3D Game Project/Assets/Scripts/Assignments/pickup/pickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour
+{
+    public int pointsToAdd;
+
+ void OnTriggerEnter(Collider other)
+ {
+     if(other.gameObject.CompareTag("Player"))
+     {
+         scoreManager.AddPoints (pointsToAdd);
+         Destroy(gameObject);
+     }
+ }
+}
---
+++ 3D Game Project/Assets/Scripts/Assignments/pickup/scoreManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scoreManager : MonoBehaviour
+{
+    public static int score;
+    // static means we cannot alter the variable, we cant create an instance of this variable
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Game Score;" + score);
+    }
+
+    public static void AddPoints(int pointsToAdd)
+    {
+        score += pointsToAdd;
+        
+    }
+}
