94661ec5360783970bf6a045905bc337c1289bea
Unknown <akalamf14@gmail.com>
2020-02-06 21:49:53 -0700

    Data types tutorial with script and notes

---
+++ 3D Game Project/Assets/Scripts/Assignments/Data Types/DataTypes.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataTypes : MonoBehaviour
+{
+
+    /* There are usually 2 categories of Data Types
+    1. VALUE - contain some value (usually numbers)
+    if the value type changes, only that specific variable is affected
+        -int
+        -float
+        -double
+        -bool
+        -char (characters)
+        -structs (value data types that simply contain one or more other variables)
+            -Vector3
+            -Quaternion
+
+    2. REFERENCE - any variable that is an object of a class
+    contain a memory address where the value is stored
+    if a reference type, all variables that contain that memory address will also be affected.
+        -classes
+            -Transform
+            -GameObject
+
+    */
+
+    void start()
+    {
+    // Value Type Example
+    Vector3 pos = transform.position;
+    pos = new Vector3(0,2,0);
+
+    // Reference Type Example
+    Transform tran = transform;
+    tran.position = new Vector3(0,2,0);
+
+    }
+
+}
