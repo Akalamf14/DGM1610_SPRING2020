de2ddb6a4eb193f358a344b4f5855ac11386baf2
Unknown <akalamf14@gmail.com>
2020-02-10 20:27:18 -0700

    Created New Character Move Script - better Organized

---
+++ 3D Game Project/Assets/Scripts/Game/CharacterMove.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    public float speed;
+    public float verticalInput;
+    public float horizontalInput;
+    public float turnSpeed;
+
+    public bool isGrounded;
+    Rigidbody gameCharacter;
+
+    public GameObject projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameCharacter = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * horizontalInput);
+        transform.Rotate(Vector3.forward * turnSpeed * Time.deltaTime * verticalInput);
+
+        if(Input.GetKeyDown(KeyCode.RightControl))
+        {
+            gameCharacter.AddForce(new Vector3(0,10,0), ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }
+}
