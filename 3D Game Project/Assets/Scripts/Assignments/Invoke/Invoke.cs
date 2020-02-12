ce6b5eed0f27ab3469adcf6208cd0ffb9cba46c6
Unknown <akalamf14@gmail.com>
2020-02-11 23:03:00 -0700

    Invoke Tutorial with script

---
+++ 3D Game Project/Assets/Scripts/Assignments/Invoke/Invoke.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invoke : MonoBehaviour
+{
+    //the Invoke functions enable you to schedule a function call after some specified time delay
+    // This allows us to build a useful method-call system, that is time sensitive
+
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnObject", 2); // the invoke function takes 2 parameters
+        //1. a string containing the name of the method we wish to call
+        //2. an amount of time to delay in seconds
+
+        //Invoke only works with methods with no parameters and have void in front
+    }
+
+    void SpawnObject() // this method or function is only meant to instantiate the GameObject at the location 0,2,0
+    {
+        Instantiate(target, new Vector3(0,2,0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 3D Game Project/Assets/Scripts/Assignments/Invoke/InvokeRepeatedly.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvokeRepeatedly : MonoBehaviour
+{
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", 2, 1);
+        //1.String with name of method
+        //2. delay in seconds before it calls the method
+        //3. delay in seconds between each subsequent call of the method
+
+        CancelInvoke("SpawnObject");
+        //in order to stop all instances of an Invoke call from this script we can use the cancel invoke method
+        //if we only wish to stop a specific Invoke, we can pass in a string containing the name of the method we wish to stop
+    }
+
+    void SpawnObject()
+    {
+        float x = Random.Range(-2.0f, 2.0f);
+        float z = Random.Range(-2.0f, 2.0f);
+       Instantiate(target, new Vector3(x,2,z), Quaternion.identity);
+
+        //function still instantiates the object, but now with a random location for x and z
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
