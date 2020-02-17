b020b2a50c90f0112f13403ebf8ca9a27ebfcb18
Unknown <akalamf14@gmail.com>
2020-02-16 22:30:04 -0700

    Minor fixes to move script

--- 3D Game Project/Assets/Scripts/Game/CharacterMove.cs
+++ 3D Game Project/Assets/Scripts/Game/CharacterMove.cs
@@ -25,24 +25,21 @@ public class CharacterMove : MonoBehaviour
         isGrounded = true;
     }
 
-    // Update is called once per frame
+        // Update is called once per frame
     void Update()
     {
         verticalInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
 
         transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * horizontalInput);
-        transform.Rotate(Vector3.forward * turnSpeed * Time.deltaTime * verticalInput);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
 
-        if(Input.GetKeyDown(KeyCode.RightControl))
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             gameCharacter.AddForce(new Vector3(0,10,0), ForceMode.Impulse);
             isGrounded = false;
         }
 
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
-        }
+      
     }
 }
