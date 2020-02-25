03e7c21a2139baa90280d2d52869afbf0647201d
Unknown <akalamf14@gmail.com>
2020-02-24 23:32:42 -0700

    Ammo Manager Script and Ammo Pickup Script

---
+++ 3D Game Project/Assets/Scripts/Game/pickup/AmmoPickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour
+{
+
+    public int ammoAmt;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+           // ammoManager.AddPoints(pointsToAdd);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ 3D Game Project/Assets/Scripts/Game/pickup/ammoManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ammoManager : MonoBehaviour
+{
+    public static int ammo;
+
+    public static void AddPoints(int pointsToAdd)
+    {
+        ammo += pointsToAdd;
+        Debug.Log("Total Ammo " + ammo);
+    }
+}
