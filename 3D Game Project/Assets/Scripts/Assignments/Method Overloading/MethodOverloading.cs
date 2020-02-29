15df72ac802104b1f8a016a0e341d85228ef8784
Unknown <akalamf14@gmail.com>
2020-02-28 21:33:52 -0700

    Method Overloading Tutorial with script

---
+++ 3D Game Project/Assets/Scripts/Assignments/Method Overloading/MethodOverloading.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MethodOverloading : MonoBehaviour
+{
+    //overloading is a process where you can give a single method multiple definitions
+    // you can use the same method name to do two different things
+
+    public int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public string Add(string str1, string str2)
+    {
+        return str1 + str2;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        SomeClass myClass = new SomeClass();
+
+        myClass.Add(1,2);
+        myClass.Add("Hello " + "World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
