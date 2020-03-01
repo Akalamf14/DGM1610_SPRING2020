1e6f6fe78af42618a127938b7f141d98dda9bdda
Unknown <akalamf14@gmail.com>
2020-02-29 22:39:46 -0700

    Generics tutorial

---
+++ 3D Game Project/Assets/Scripts/Assignments/Generics/Generics.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generics : MonoBehaviour
+{
+    public T GenericMethod<T>(T param)// you can add where T : (class, struct, new(), Monobehaviour, IEnumerable)
+   
+   {
+        return param;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
