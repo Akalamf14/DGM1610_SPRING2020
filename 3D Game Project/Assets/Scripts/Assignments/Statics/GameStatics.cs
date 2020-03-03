d58865ce6a0ece1d20a0ecc754f5c738f505220c
Unknown <akalamf14@gmail.com>
2020-03-02 17:09:55 -0700

    More work done with EnemyHealth and EnemyMove script. Created tag for Enemy

--- 3D Game Project/Assets/Scripts/Assignments/Statics/GameStatics.cs
+++ 3D Game Project/Assets/Scripts/Assignments/Statics/GameStatics.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -22,5 +22,6 @@ public class GameStatics : MonoBehaviour
         
     }
 }
+*/
 
 
--- 3D Game Project/Assets/Scripts/Assignments/Statics/Statics.cs
+++ 3D Game Project/Assets/Scripts/Assignments/Statics/Statics.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -27,3 +27,4 @@ public class Statics : MonoBehaviour
     // Static means you can't create instances of that variable or class
 
 }
+*/
