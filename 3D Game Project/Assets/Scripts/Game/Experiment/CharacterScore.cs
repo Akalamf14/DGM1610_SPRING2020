851cb9bae018ea4c3b3981550b96dffe7151d326
Unknown <akalamf14@gmail.com>
2020-03-06 23:27:37 -0700

    Work in process - UI Text for player score with script

---
+++ 3D Game Project/Assets/Scripts/Game/Experiment/CharacterScore.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CharacterScore : MonoBehaviour
+{
+    public static int score;
+    public int winScore;
+
+    public Text winText;
+
+    public Text scoreText;
+    
+    void Awake()
+    {
+        Time.timeScale = 1;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+
+        score = 0;
+
+        winText.GetComponent<Text>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(score < 0)
+        {
+            score = 0;
+            scoreText.text = " " + score;
+
+            if(score >= winScore)
+            {
+                print("Win Score Reached = " + score);
+                winText.GetComponent<Text>().enabled = true;
+                Time.timeScale = 0;
+            }
+        }
+        
+    }
+
+    public static void AddPoints(int PointsToAdd)
+    {
+        score += PointsToAdd;
+    }
+}
