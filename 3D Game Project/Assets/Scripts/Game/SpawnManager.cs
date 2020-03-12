b74608acc64f28ce2330cd610adbd6abf5aa78ea
Unknown <akalamf14@gmail.com>
2020-03-11 22:25:25 -0600

    Spawn Manager Update - Used For Loop to create enemy index

--- 3D Game Project/Assets/Scripts/Game/SpawnManager.cs
+++ 3D Game Project/Assets/Scripts/Game/SpawnManager.cs
@@ -7,15 +7,20 @@ public class SpawnManager : MonoBehaviour
     public GameObject [] enemyPrefab;
     public int enemyIndex;
 
+    public int enemiesToSpawn;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       //InvokeRepeating("SpawnRandomEnemy", startDelay, spawnInterval);
+
+       SpawnEnemyWave(); 
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         int enemyIndex = Random.Range(0, enemyPrefab.Length);
 
         
@@ -23,6 +28,16 @@ public class SpawnManager : MonoBehaviour
         {
             Instantiate(enemyPrefab[enemyIndex], new Vector3(0,14,0), enemyPrefab[enemyIndex].transform.rotation);
 
+        }
+        */
+
+    }
+
+    void SpawnEnemyWave()
+    {
+        for( int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab[enemyIndex], transform.position, enemyPrefab[enemyIndex].transform.rotation);
         }
 
     }
