0d714a4387f65f82fc0f4705d1dbce2f525b9174
Unknown <akalamf14@gmail.com>
2020-03-18 21:35:17 -0600

    Overriding Tutorial with fruit, apple, and fruitsalad scripts

---
+++ 3D Game Project/Assets/Scripts/Assignments/Overriding/FruitSaldad1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSalad1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Apple1 myApple = new Apple1();
+
+        //The apple version of the methods override the fruit versions.
+        // Since the apple versions call the fruit version with the 'base' keyword, both are called
+
+        myApple.SayHello();
+        myApple.Chop();
+
+        //since the methods of the fruit class are virtual and the methods of the apple class are override
+        //when we upcast an apple into a fruit, the apple version of the methods are used
+
+        Fruit1 myFruit = new Apple1();
+        myFruit.SayHello();
+        myFruit.Chop();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
