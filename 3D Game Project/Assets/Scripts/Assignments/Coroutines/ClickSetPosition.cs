8f5b3a37b50a15754377b3f28d9af4840725aaf2
Unknown <akalamf14@gmail.com>
2020-03-20 21:52:52 -0600

    Coroutines Unity Tutorial with scripts

---
+++ 3D Game Project/Assets/Scripts/Assignments/Coroutines/ClickSetPosition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickSetPosition : MonoBehaviour
+{
+    public PropertiesAndCoroutines coroutineScript;
+
+    void OnMouseDown()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        Physics.Raycast(ray, out hit);
+
+        if(hit.collider.gameObject == gameObject)
+        {
+            Vector3 newTarget = hit.point + new Vector3(0, 0.5f, 0);
+
+            coroutineScript.Target = newTarget;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
