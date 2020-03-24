8369b1c2003552da7575999bc69f50535a1950c8
Unknown <akalamf14@gmail.com>
2020-03-23 21:52:37 -0600

    More practice with move scripts and Vector3

---
+++ 3D Game Project/Assets/Scripts/Assignments/Move/PlayerMovePractice.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovePractice : MonoBehaviour
+{
+    public Vector3 startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Transform.position = new Vector3(0,0,0);
+        // we can make position x, y, or z
+        // Transform.rotation
+        // Transform.scale
+        
+        transform.position = startPosition;
+        startPosition = new Vector3(0,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
