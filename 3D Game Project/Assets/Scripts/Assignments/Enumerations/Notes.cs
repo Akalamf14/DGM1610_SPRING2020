bfb9714d17040ac26f33214f1dbbfaf793dc858c
Unknown <akalamf14@gmail.com>
2020-03-25 17:24:40 -0600

    enums and switch statements

---
+++ 3D Game Project/Assets/Scripts/Assignments/Enumerations/Notes.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    // enums are customizable types
+    //defining a type and a subset within that type.
+    // enums is the type
+    // Days is the title of the enumeration
+
+    enum Days{sun, mon, tues, wed, thurs, fri, sat};
+    // defalt sun = 0, mon = 1, tues = 2, etc
+    // can change each to personal number
+    // sun = 10, mon = 145, etc
+
+    Days currentDay;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentDay = Days.sun;
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentDay == Days.mon)
+        {
+            print("go back to bed.");
+        }
+    }
+}
---
+++ 3D Game Project/Assets/Scripts/Assignments/Switch Statements/Switch.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    //switches are used when there a lot of options
+    // mpc shop - with lots of items to buy
+    // use types ex: different types of potions you could buy
+
+    public string favHero;
+
+    public int smarts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //switch as an if conditional
+        switch(favHero)
+        {
+            case "Superman":
+            print("wrong!");
+            break;
+
+            case "Thor":
+            print("correct!");
+            break;
+
+            case "Batman":
+            print("Correct!");
+            break;
+
+            default:
+            print("ERROR");
+            break;
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(smarts)
+        {
+            case 1:
+            print("You're dumb");
+            break;
+            
+            case 5:
+            print("You're smart");
+            break;
+
+            default:
+            print("derp!");
+            break;
+
+        }
+    }
+}
