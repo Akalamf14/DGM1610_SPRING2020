43ddd6e948b5afc23c47fb5151f411b3ed1c8818
Unknown <akalamf14@gmail.com>
2020-03-28 23:06:45 -0600

    Practice script using switch statements

---
+++ 3D Game Project/Assets/Scripts/Assignments/Switch Statements/SwitchTwo.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchTwo : MonoBehaviour
+{
+    public string animals;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch(animals)
+        {
+            case "horse":
+            print("Mammal");
+            break;
+
+            case "beetle":
+            print("insect");
+            break;
+
+            case "frog":
+            print("amphibian");
+            break;
+
+            case "cat":
+            print("mammal");
+            break;
+
+            case "lizard":
+            print("reptile");
+            break;
+
+            case "canary":
+            print("bird");
+            break;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
