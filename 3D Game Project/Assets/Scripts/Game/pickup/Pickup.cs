44d3e672df8793494bbd106a066864f5c46217ec
Unknown <akalamf14@gmail.com>
2020-03-30 19:08:35 -0600

    UI Text with score manager

--- 3D Game Project/Assets/Scripts/Game/pickup/Pickup.cs
+++ 3D Game Project/Assets/Scripts/Game/pickup/Pickup.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class Pickup : MonoBehaviour
 {
     public int PointsToAdd;
-    public string pickuptype;
+    //public string pickuptype;
     
 
  void OnTriggerEnter(Collider other)
  {
      if(other.gameObject.CompareTag("Player"))
      {
-        CharacterScore.AddPoints(PointsToAdd);
+        ScoreManager.AddPoints(PointsToAdd);
         Destroy(gameObject);
      }
  }
+
+ // compareTag is good to add for various players or various enemy that all have the same tag
+ //compareTag looks for the tag given to each player or object
+ // find is good for individual player or specific objects - looks for the name of your character or game object
+ // with find make sure spelling is exactly the same with the same capitalization as well
+
 }
--- 3D Game Project/Assets/Scripts/Game/pickup/scoreManager.cs
+++ 3D Game Project/Assets/Scripts/Game/pickup/scoreManager.cs
@@ -1,29 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class scoreManager : MonoBehaviour
-{/*
+{
     public static int score;
+    public int winScore;
     // static means we cannot alter the variable, we cant create an instance of this variable
 
+    public Text winText;
+    // Green means its a type, light blue means its a variable
+
+    private Text scoreText;
+
+    void Awake()
+    {
+        Time.timeScale = 1;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        scoreText = GetComponent<Text>();
+
+        score = 0;
+
+        winText.GetComponent<Text>().enabled = false;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+       if(score < 0)
+       {
+           score = 0;
+       }
+
+       scoreText.text =  " " + score;
+
+       if(score >= winScore)
+       {
+           print("Win Score Reached = " + score);
+           winText.GetComponent<Text>().enabled = true;
+           Time.timeScale = 0;
+       }
+
+       if(Input.GetKeyDown(KeyCode.Escape))
+       {
+           SceneManager.LoadScene(0);
+       }
     }
 
     public static void AddPoints(int pointsToAdd)
     {
         score += pointsToAdd;
-        Debug.Log("Game Score;" + score);
+        
     }
-    */
+    
 }
