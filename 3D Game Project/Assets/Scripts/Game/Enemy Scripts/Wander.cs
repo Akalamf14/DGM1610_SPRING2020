46ffda9ba4b415608006809236606f6ac07c89e6
Unknown <akalamf14@gmail.com>
2020-04-06 21:11:16 -0600

    Wander Script with navMesh

---
+++ 3D Game Project/Assets/Scripts/Game/Enemy Scripts/Wander.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Wander : MonoBehaviour
+{
+    public float wanderRadius;
+    public float wanderTime;
+
+    private Transform target;
+    private NavMeshAgent agent;
+    private float timer;
+
+    void OnEnable()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        timer = wanderTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer >= wanderTime)
+        {
+            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+            agent.SetDestination(newPos);
+            timer = 0;
+        }
+    }
+
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+
+        randDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
+    }
+}
