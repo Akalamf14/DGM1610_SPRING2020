d07b7d7a0a2c28a5cf33f03aa34163a020a45ffe
Unknown <akalamf14@gmail.com>
2020-04-08 16:57:38 -0600

    Scriptable Objects

---
+++ 3D Game Project/Assets/Scripts/Game/SO.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New SO", menuName = "Scriptable Object / New Object")]
+
+  
+public class SO : ScriptableObject // manage data
+// Sriptable objects are like containers to hold data
+// its not a game object that is used in the scene
+{
+   
+    public new string name;
+
+    
+    public string description;
+
+    
+    public GameObject model;
+
+    
+    public Color color;
+
+    // scriptable objects do not use monobehavior callbacks suck as start() and update()
+
+    //scriptable objects can also contain function and conditionals.
+
+    /*private void PrintOut(string name, string description)
+    {
+        Debug.Log(name + " " + description);
+
+        if()
+    }
+
+    */
+
+
+
+}
---
+++ 3D Game Project/Assets/Scripts/Game/TestObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObject : MonoBehaviour
+{
+
+    public SO data;
+
+    private GameObject model;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        model = data.model;
+
+        var modelRend = model.GetComponent<Renderer>();
+        modelRend.sharedMaterial.SetColor("_Color", data.color);
+
+        print(data.name);
+        print(data.description);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
