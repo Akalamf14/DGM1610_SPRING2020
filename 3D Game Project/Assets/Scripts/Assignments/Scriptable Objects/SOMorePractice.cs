1f0348ad66547b9bb44a9d3d08200355911f6a66
Unknown <akalamf14@gmail.com>
2020-04-11 22:56:00 -0600

    Practice with Scriptable Objects

---
+++ 3D Game Project/Assets/Scripts/Assignments/Scriptable Objects/SOMorePractice.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Another SO", menuName = "Second Scriptable Object / New Candy")]
+
+public class SOMorePractice : ScriptableObject
+{
+    public string candyType;
+
+    public string candyFlavor;
+
+    public bool rating;
+
+    private void PrintOut(string candyType, string candyFlavor, bool rating)
+    {
+        if(rating == true)
+        {
+            Debug.Log(candyFlavor + candyType + "Is really good");
+        }
+        else
+        {
+            Debug.Log(candyFlavor + candyType + "Is really bad");
+        }
+    }
+
+    
+    
+}
